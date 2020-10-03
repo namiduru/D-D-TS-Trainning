1f1558605da7cf73e88fe72eaa8fe00c0e2355c3
Mehmet NAMIDURU <mehmetnamiduru1@jotforms-MacBook-Pro-9.local>
2020-10-03 20:10:52 +0300

    TS :: Interacting with DOM Elements

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = <HTMLTemplateElement>(
@@ -11,11 +14,28 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = <HTMLInputElement>this.element.querySelector('#title');
+    this.descriptionInputElement = <HTMLInputElement>this.element.querySelector('#description');
+    this.peopleInputElement = <HTMLInputElement>this.element.querySelector('#people');
+
+    this.configure();
     this.attach();
   }
 
   private attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault(); // To Stop HTTP Execution, it is browsers default execution logic...
+    console.log(this.titleInputElement);
+  }
+
+  private configure() {
+    // Callback has its own context so we should pass this to access object properties from callback function
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
   }
 }
 
