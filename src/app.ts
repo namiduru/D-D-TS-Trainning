6d027ffefa5bbeb5d4275658f4c107d04a1d956b
Mehmet Namıduru <mehmetnamiduru@gmail.com>
2020-10-08 00:33:29 +0300

    TS :: Rendering Project Items with Class

--- src/app.ts
+++ src/app.ts
@@ -132,6 +132,28 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>{
+  private project: Project;
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {
+
+  }
+
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.project.people.toString();
+    this.element.querySelector('p')!.textContent = this.project.title;
+  }
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement>{
   assignedProjects: Project[];
@@ -159,9 +181,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>{
     const listEl = document.getElementById(`${this.type}-project-list`)! as HTMLUListElement;
     listEl.innerHTML = ''; // Clear the inner html and rendering from the filitered store again
     for(const project of this.assignedProjects) {
-      const li = document.createElement('li');
-      li.textContent = project.title;
-      listEl.appendChild(li);
+      new ProjectItem(this.element.querySelector('ul')!.id, project);
     }
   }
 
