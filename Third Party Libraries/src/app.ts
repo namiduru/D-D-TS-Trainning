66a2dec50aff954cc02a6a182b4957fd4efc9a9e
Mehmet Namıduru <mehmetnamiduru@gmail.com>
2020-10-19 10:51:38 +0300

    TS :: 3rd Party :: accessing dom elements

--- Third Party Libraries/src/app.ts
+++ Third Party Libraries/src/app.ts
@@ -1,1 +1,11 @@
-// Code goes here!
+const form = document.querySelector('form')!;
+const adressInput = document.getElementById('adress')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAdress = adressInput.value;
+
+  // send this to Google's API!
+}
+
+form.addEventListener('submit', searchAddressHandler);
