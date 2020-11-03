b8049ee0669f766b85a373d4f9a8691419e190ec
Mehmet Namıduru <mehmetnamiduru@gmail.com>
2020-11-03 10:26:10 +0300

    TS :: Working with Controllers & Parsing Request Nodies

--- Node Project/src/app.ts
+++ Node Project/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import todoRoutes from './routes/todos';
+import {json} from 'body-parser';
 
 const app = express();
 
+app.use(json());
 app.use('/todos', todoRoutes);
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
   res.status(500).json({message: err.message});
