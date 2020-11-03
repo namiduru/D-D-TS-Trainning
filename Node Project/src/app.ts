2eb0fda5f00261ed21778d56e393ad72ebfdfacf
Mehmet Namıduru <mehmetnamiduru@gmail.com>
2020-11-03 09:11:32 +0300

    TS :: Adding Middleware & Types

--- Node Project/src/app.ts
+++ Node Project/src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import todoRoutes from './routes/todos';
 
 const app = express();
+
+app.use('/todos', todoRoutes);
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.status(500).json({message: err.message});
+});
 
 app.listen(3000);
